3f98290ab7cc3c3773eb5d3fef7c231384891317
Kaleth Alexander Benjumea Hernandez <113316840+kalethabh@users.noreply.github.com>
2023-10-12 02:45:15 +0000

    Ruta get, post, delet

--- app.py
+++ app.py
@@ -5,16 +5,19 @@ app = Flask(__name__, template_folder='templates')
 # Lista para almacenar voluntarios (simulación de una base de datos)
 voluntarios_db = []
 
+
 @app.route("/")
 def index():
     print('Request for index page received')
     return render_template("Home.html")
 
-@app.route('/voluntario')
+
+@app.route('/register-voluntario')
 def voluntario():
     return render_template('voluntario.html')
 
-@app.route('/agrevoluntario', methods=['POST'])
+
+@app.route('/create-voluntario', methods=['POST'])
 def add_voluntario():
     ID = request.form.get('ID')
     Nombre = request.form.get('name')
@@ -32,6 +35,24 @@ def add_voluntario():
 
     voluntarios_db.append(nuevo_voluntario)
     return jsonify({"mensaje": "Voluntario agregado con éxito", "nuevo_voluntario": nuevo_voluntario})
+
+# Ruta para eliminar voluntario por ID
+
+
+@app.route('/delete-voluntario/<id>', methods=['GET'])
+def delete_voluntario(id):
+    for voluntario in voluntarios_db:
+        if voluntario['ID'] == id:
+            voluntarios_db.remove(voluntario)
+            return jsonify({"mensaje": "Voluntario eliminado con éxito"})
+    return jsonify({"mensaje": "Voluntario no encontrado"})
+
+
+# Ruta para mostrar todos los voluntarios
+@app.route('/voluntarios', methods=['GET'])
+def mostrar_voluntarios():
+    return jsonify({"voluntarios": voluntarios_db})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
