0e19113b1ad82ed343f4388180130dd56f537714
kalethabh <kalethabh@gmail.com>
2023-10-19 20:56:08 -0500

    Deploy

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask, request, render_template, jsonify
+from fastapi import FastAPI, Request, Form
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse, JSONResponse
+import uvicorn
 
-app = Flask(__name__, template_folder='templates')
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
 # Lista para almacenar voluntarios (simulación de una base de datos)
 voluntarios_db = []
@@ -9,83 +13,57 @@ voluntarios_db = []
 programas_db = []
 
 # Ruta para mostrar la página principal
-
-
-@app.route("/")
-def index():
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
     print('Request for index page received')
-    return render_template("Home.html")
+    return templates.TemplateResponse('Home.html', {"request": request})
 
 # Ruta para mostrar formulario de registro de voluntario
-
-
-@app.route('/register-voluntario')
-def voluntario():
-    return render_template('FormularioVoluntario.html')
+@app.get('/register-voluntario', response_class=HTMLResponse)
+async def voluntario(request: Request):
+    return templates.TemplateResponse('FormularioVoluntario.html', {"request": request})
 
 # Ruta para registrar un voluntario
-
-
-@app.route('/create-voluntario', methods=['POST'])
-def add_voluntario():
-    ID = request.form.get('ID')
-    Nombre = request.form.get('Nombre')
-    Apellido = request.form.get('Apellido')
-    Telefono = request.form.get('Telefono')
-    Intereses = request.form.get('Intereses')
-
+@app.post('/create-voluntario', response_class=JSONResponse)
+async def add_voluntario(ID: str = Form(...), Nombre: str = Form(...), Apellido: str = Form(...), Telefono: str = Form(...), Intereses: str = Form(...)):
     nuevo_voluntario = {
         'ID': ID,
-        'name': Nombre,
+        'Nombre': Nombre,
         'Apellido': Apellido,
         'Telefono': Telefono,
         'Intereses': Intereses
     }
 
     voluntarios_db.append(nuevo_voluntario)
-    return jsonify({"mensaje": "Voluntario agregado con éxito", "nuevo_voluntario": nuevo_voluntario})
+    return {"mensaje": "Voluntario agregado con éxito", "nuevo_voluntario": nuevo_voluntario}
 
 # Ruta para eliminar voluntario por ID
-
-
-@app.route('/eliminar-voluntario', methods=['GET'])
-def form_delete_voluntario():
-    return render_template('FormDeleteVoluntario.html')
+@app.get('/eliminar-voluntario', response_class=HTMLResponse)
+async def form_delete_voluntario(request: Request):
+    return templates.TemplateResponse('FormDeleteVoluntario.html', {"request": request})
 
 # Ruta para eliminar voluntario por ID
-
-
-@app.route('/eliminar-voluntario', methods=['POST'])
-def delete_voluntario():
-    ID = request.form.get('ID')
+@app.post('/eliminar-voluntario', response_class=JSONResponse)
+async def delete_voluntario(ID: str = Form(...)):
     for voluntario in voluntarios_db:
         if voluntario['ID'] == ID:
             voluntarios_db.remove(voluntario)
-            return jsonify({"mensaje": "Voluntario eliminado con éxito"})
-    return jsonify({"mensaje": "Voluntario no encontrado"})
+            return {"mensaje": "Voluntario eliminado con éxito"}
+    return {"mensaje": "Voluntario no encontrado"}
 
 # Ruta para mostrar todos los voluntarios
-
-
-@app.route('/voluntarios', methods=['GET'])
-def mostrar_voluntarios():
-    return jsonify({"voluntarios": voluntarios_db})
+@app.get('/voluntarios', response_class=JSONResponse)
+async def mostrar_voluntarios():
+    return {"voluntarios": voluntarios_db}
 
 # Ruta para mostrar formulario de registro de programa
-
-
-@app.route('/register-programa')
-def programa():
-    return render_template('FormularioPrograma.html')
+@app.get('/register-programa', response_class=HTMLResponse)
+async def programa(request: Request):
+    return templates.TemplateResponse('FormularioPrograma.html', {"request": request})
 
 # Ruta para registrar un programa
-
-
-@app.route('/create-programa', methods=['POST'])
-def add_programa():
-    nombre = request.form.get('nombre')
-    descripcion = request.form.get('descripcion')
-
+@app.post('/create-programa', response_class=JSONResponse)
+async def add_programa(nombre: str = Form(...), descripcion: str = Form(...)):
     nuevo_programa = {
         'nombre': nombre,
         'descripcion': descripcion,
@@ -93,45 +71,29 @@ def add_programa():
     }
 
     programas_db.append(nuevo_programa)
-    return jsonify({"mensaje": "Programa agregado con éxito", "nuevo_programa": nuevo_programa})
+    return {"mensaje": "Programa agregado con éxito", "nuevo_programa": nuevo_programa}
 
 # Ruta para mostrar todos los programas
-
-
-@app.route('/programas', methods=['GET'])
-def mostrar_programas():
-    return jsonify({"programas": programas_db})
+@app.get('/programas', response_class=JSONResponse)
+async def mostrar_programas():
+    return {"programas": programas_db}
 
 # Ruta para mostrar formulario de registro en programa
-
-
-@app.route('/asignar-programa')
-def AsignarPrograma():
-    return render_template('AsignarPrograma.html')
+@app.get('/asignar-programa', response_class=HTMLResponse)
+async def AsignarPrograma(request: Request):
+    return templates.TemplateResponse('AsignarPrograma.html', {"request": request})
 
 # Ruta para que los voluntarios se unan a un programa
-
-
-@app.route('/unirse-programa', methods=['POST'])
-def unirse_programa():
-    programa_id = request.form.get('programa_id')
-    voluntario_id = request.form.get('voluntario_id')
-
-    # Buscar el programa por su nombre
-    programa_encontrado = next(
-        (programa for programa in programas_db if programa['nombre'] == programa_id), None)
-
-    # Buscar el voluntario por su ID
-    voluntario_encontrado = next(
-        (voluntario for voluntario in voluntarios_db if voluntario['ID'] == voluntario_id), None)
+@app.post('/unirse-programa', response_class=JSONResponse)
+async def unirse_programa(programa_id: str = Form(...), voluntario_id: str = Form(...)):
+    programa_encontrado = next((programa for programa in programas_db if programa['nombre'] == programa_id), None)
+    voluntario_encontrado = next((voluntario for voluntario in voluntarios_db if voluntario['ID'] == voluntario_id), None)
 
     if programa_encontrado and voluntario_encontrado:
         programa_encontrado['participantes'].append(voluntario_encontrado)
-        return jsonify({"mensaje": "Voluntario agregado al programa con éxito"})
+        return {"mensaje": "Voluntario agregado al programa con éxito"}
     else:
-        return jsonify({"mensaje": "Programa o voluntario no encontrado"})
-
+        return {"mensaje": "Programa o voluntario no encontrado"}
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
-
+    uvicorn.run('app:app')
