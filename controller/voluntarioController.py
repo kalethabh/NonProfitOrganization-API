d81caaea14ea9a6a9981e6e953f21fe0c1a179ee
kalethabh <kalethabh@gmail.com>
2023-10-20 08:50:33 -0500

    Deploy

--- controller/voluntarioController.py
+++
@@ -1,9 +0,0 @@
-class voluntarioController():
-    def add():
-        with open(self.file, 'r+', encoding='utf-8') as f:
-            data = json.load(f)
-            data.append(new_student.__str__())
-            f.seek(0)
-            json.dump(data, f)
-        f.close()
-        return new_student
